800b7f67c0bb1f9f8b8e6f562949416d4a28117f
YangHao2233 <jerry0102@foxmail.com>
2022-02-17 16:35:11 +0800

    initial files. including tlstm.nn construct and config files

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @Author: yanghao
+# @Create time: 2022/01/28 13:11
+# @Description: VTI-LSTM 网络架构超参数
+
+epoches = 10
+classes = 2
+batch_size = 20
+learning_rate = 0.0001
+
+ultrasonic_path = './data/B超数据—0121.csv'
+birthseize_path = './data/B超数据—0121.csv'
